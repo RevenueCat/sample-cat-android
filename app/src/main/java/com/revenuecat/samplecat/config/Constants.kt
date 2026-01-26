d6464b3b253baea2712f1e4d5e3e5baf7f29c3e7
Mustapha Tarek Ben Lechhab <tarek@revenuecat.com>
2026-01-26 21:32:39 +0100

    Make constants internal

--- app/src/main/java/com/revenuecat/samplecat/config/Constants.kt
+++ app/src/main/java/com/revenuecat/samplecat/config/Constants.kt
@@ -5,16 +5,16 @@ import com.revenuecat.samplecat.BuildConfig
 /**
  * Configuration constants for the app's RevenueCat settings.
  */
-object Constants {
+internal object Constants {
     /**
      * The API key for your app from the RevenueCat dashboard.
      * @see <a href="https://app.revenuecat.com">RevenueCat Dashboard</a>
      */
-    const val API_KEY: String = BuildConfig.REVENUECAT_API_KEY
+    internal const val API_KEY: String = BuildConfig.REVENUECAT_API_KEY
 
     /**
      * The entitlement identifier from the RevenueCat dashboard that is activated
      * upon successful in-app purchase for the duration of the purchase.
      */
-    val entitlementId: String? = BuildConfig.ENTITLEMENT_ID.takeIf { it.isNotBlank() }
+    internal val entitlementId: String? = BuildConfig.ENTITLEMENT_ID.takeIf { it.isNotBlank() }
 }
